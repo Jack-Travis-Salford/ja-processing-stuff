5568f010ac87663f64385f9a617624ced9b880b1
jack <jacktrav28@gmail.com>
2026-09-09 17:21:55 +0100

    approx kanji acceptance list

--- mecab/most_common_kanji_calc.py
+++ mecab/most_common_kanji_calc.py
@@ -1,3 +1,7 @@
+kanji_chars = set()
+kanji_chars.update([chr(s) for s in range(0x3400, 0x4DBF)])
+kanji_chars.update([chr(s) for s in range(0x4E00, 0x9FFF)])
+kanji_chars.update([chr(s) for s in range(0xF900, 0xFAFF)])
 
 
 # Read words
